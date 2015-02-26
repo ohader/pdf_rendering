d8b443f2c74a152cdfac278617dc62cd8a2f7626
Oliver Hader <oliver@typo3.org>
2015-02-26 18:43:36 +0100

    [TASK] Add possibility to initialize custom font

---
+++ Classes/ViewHelpers/Initialize/FontViewHelper.php
@@ -0,0 +1,34 @@
+<?php
+namespace OliverHader\PdfRendering\ViewHelpers\Initialize;
+
+/**
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+use OliverHader\PdfRendering\Utility\ZendPdfUtility;
+use OliverHader\PdfRendering\ViewHelpers\AbstractDocumentViewHelper;
+
+/**
+ * Class TotalViewHelper
+ * @author Oliver Hader <oliver.hader@typo3.org>
+ */
+class FontViewHelper extends AbstractDocumentViewHelper {
+
+	/**
+	 * @param string $fontFile
+	 * @return void
+	 */
+	public function render($fontFile) {
+		$font = ZendPdfUtility::getFont($fontFile);
+	}
+
+}
